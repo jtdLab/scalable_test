96b818d2fc138c39f6d93b4df844f52328cf4b7e
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-05-09 14:15:53 +0200

    mocktail

--- lib/src/presentation/presentation_test.dart
+++ lib/src/presentation/presentation_test.dart
@@ -4,10 +4,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
-export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:bloc_test/bloc_test.dart';
 export 'package:flutter_test/flutter_test.dart';
 export 'package:golden_toolkit/golden_toolkit.dart';
+export 'package:mocktail/mocktail.dart';
 
 part 'core.dart';
-part 'page_test.dart';
 part 'flow_test.dart';
+part 'page_test.dart';
